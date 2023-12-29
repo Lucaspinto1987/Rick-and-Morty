ad5c1c9f93b1123adbf669e023ea4fd71088cf17
Lucaspinto1987 <128609499+Lucaspinto1987@users.noreply.github.com>
2023-12-29 16:41:45 +0100

    Actualice la app, utilizando archivos y componentes que tenia en un repo anterior que no podia localizar

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -24,13 +24,13 @@ const Detail = () => {
 
     return (
         <div>
-     {/*        <h2> {character?.name} </h2>
+            <h2> {character?.name} </h2>
             <h2> {character?.status} </h2>
             <h2> {character?.species} </h2>
             <h2> {character?.gender} </h2>
             <h2> {character?.origin.name}</h2>
             <img sc r={character?.image} alt={character?.name} />
- */}
+
 
         </div>
     )
